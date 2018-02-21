3f4bb37dfebabc85f5c61c5a8da2cda5e56a8dbd
Vilius <vpranckaitis@gmail.com>
2018-02-21 23:25:42 +1100

    Refactored code

--- src/main/scala/lt/vpranckaitis/akka/stream/example/GithubReplay.scala
+++ src/main/scala/lt/vpranckaitis/akka/stream/example/GithubReplay.scala
@@ -1,12 +1,12 @@
 package lt.vpranckaitis.akka.stream.example
 
-
 import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.ContentTypes.`text/plain(UTF-8)`
 import akka.http.scaladsl.model.headers._
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpRequest, HttpResponse}
+import akka.http.scaladsl.model.{HttpEntity, HttpRequest, HttpResponse}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream._
@@ -17,42 +17,36 @@ import spray.json.DefaultJsonProtocol._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import scala.util.{Success, Try}
+
+sealed trait Response
+case class NextPage(uri: String) extends Response
+case class PullRequest(title: String, state: String, createdAt: String, merged_at: Option[String]) extends Response
 
 object GithubReplay extends App {
   implicit val system: ActorSystem = ActorSystem()
   implicit val mat: Materializer = ActorMaterializer()
 
-  sealed trait Response
-  case class NextPage(uri: String) extends Response
-  case class Commit(message: String) extends Response
-  case class PullRequest(title: String, state: String, createdAt: String, merged_at: Option[String]) extends Response
-
   implicit val pullRequestFormat = jsonFormat(PullRequest, "title", "state", "created_at", "merged_at")
 
-  val token = args(0)
+  val httpRequest: Flow[String, Response, NotUsed] = {
+    val connectionPool = Http().cachedHostConnectionPoolHttps[Unit](host = "api.github.com")
 
-  val connectionPool = Http().cachedHostConnectionPoolHttps[Unit](host = "api.github.com")
+    val authHeader = args.headOption map { token => Authorization(GenericHttpCredentials("token", token)) }
 
-  def httpRequest: Flow[String, Response, NotUsed] = {
-    def toRequest(uri: String) = {
-      val headers = List(Authorization(GenericHttpCredentials("token", token, Map())))
-      HttpRequest(uri = uri, headers = headers)
+    def toRequest(uri: String) = HttpRequest(uri = uri).withHeaders(authHeader.toList)
+
+    def getNext(linkHeader: Link): Option[NextPage] = linkHeader.values.collectFirst {
+      case LinkValue(uri, params) if params.exists(_.value == "next") => NextPage(uri.toString)
     }
-    def getNextPage(linkHeader: Option[Link]): Option[NextPage] = linkHeader
-      .flatMap { _.values.find { _.params.exists(_.value() == "next") } }
-      .map { link => NextPage(link.uri.toString) }
-    def parseResponse(r: (Try[HttpResponse], Unit)) = r match {
-      case (Success(resp), _) => Unmarshal(resp.entity).to[List[PullRequest]].map { js => (resp.header[Link], js) }
-      case _ => Future.failed(new RuntimeException())
-    }
-    def createMessages(x: (Option[Link], List[PullRequest])) = getNextPage(x._1) ++: x._2
+
+    def parseResponse(resp: HttpResponse) =
+      Unmarshal(resp.entity).to[List[PullRequest]].map { prs => resp.header[Link].flatMap(getNext) ++: prs }
 
     Flow[String]
       .map(uri => (toRequest(uri), ()))
       .via(connectionPool)
-      .mapAsync(10)(parseResponse)
-      .mapConcat(createMessages)
+      .mapAsync(1)(_._1.fold(Future.failed, parseResponse))
+      .mapConcat(identity)
   }
 
   val traversePages = Flow.fromGraph(GraphDSL.create() { implicit builder =>
@@ -72,29 +66,23 @@ object GithubReplay extends App {
     FlowShape(merge.in(1), output.outlet)
   })
 
-  def parseMonthAndDuration(pr: PullRequest): Option[(String, Int)] = pr match {
+  def parseMonthAndDuration: PartialFunction[PullRequest, (String, Int)] = {
     case PullRequest(_, "closed", created, Some(merged)) =>
       val createdDate = DateTime.parse(created)
       val hours = Hours.hoursBetween(createdDate, DateTime.parse(merged)).getHours
-      Some((createdDate.toString("YYYY-MM"), hours))
-    case _ => None
+      (createdDate.toString("YYYY-MM"), hours)
   }
 
   def flow(user: String, repo: String) =
     Source.single(s"https://api.github.com/repos/$user/$repo/pulls?state=closed&sort=created&direction=asc&per_page=300")
       .via(traversePages)
-      //.map(x => {println(x + "\n" + parseMonthAndDuration(x)); x})
-      .async
-      .map(parseMonthAndDuration)
-      .collect { case Some(x) => x }
+      .collect(parseMonthAndDuration)
       .sliding(2)
-      .splitAfter(SubstreamCancelStrategy.propagate){
+      .splitAfter(SubstreamCancelStrategy.drain) {
         case (month1, _) +: (month2, _) +: _ => month1 != month2
         case _ => false
       }
-      .map { _.head }
-      .fold(("", List.empty[Int])) { case (acc, x) => (x._1, x._2 :: acc._2) }
-      .concatSubstreams
+      .fold(("", List.empty[Int])) { case ((_, hs), (m, h) +: _) => (m, h :: hs) }
       .map { case (month, hours) =>
         val cnt = hours.size
         val mean = hours.sum.toDouble / cnt
@@ -102,14 +90,12 @@ object GithubReplay extends App {
         val max = hours.max
         (month, cnt, mean, median, max)
       }
+      .concatSubstreams
 
   val route = get {
     path("users" / Segment / "repos" / Segment) { (user, repo) =>
       complete {
-        val entity = HttpEntity.Chunked.fromData(
-          ContentTypes.`text/plain(UTF-8)`,
-          flow(user, repo).map { x => ByteString(x + "\n") })
-        HttpResponse(entity = entity)
+        HttpEntity.Chunked.fromData(`text/plain(UTF-8)`, flow(user, repo).map { x => ByteString(x + "\n") })
       }
     }
   }
